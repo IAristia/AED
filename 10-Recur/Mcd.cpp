235d778c450a8f0841e077c2b65e90bdbd1c14c0
Ezequiel_Luberto <eluberto@frba.utn.edu.ar>
2022-08-18 12:23:48 -0300

    Implementacion+assert

--- 10-Recur/Mcd.cpp
+++ 10-Recur/Mcd.cpp
@@ -2,16 +2,27 @@
 #include <iostream>
 #include <cassert>
 
+/*Especificacion mcd
 
-//Especificacion mcd
+ Mcd: Z x Z --> Z / Mcd(d,n)= {0      r = 0
+                              {Mcd (b,r) si r != 0
+ r = n%d
+ r == resto de la division de n por d
 
-// Mcd: Z x Z --> Z / Mcd(d,n)= {0      r = 0
-//                              {Mcd (b,r) si r != 0
-// r = n%d
-// r == resto de la division de n por d
+*/
 
+/*Prototipo*/
+int mcd(int, int);
 
-//Prototipo
+/*Assert*/
+int main()
+{
+    assert(mcd(14, 2) == 2);
+    assert(mcd(13, 7) == 1);
+    assert(mcd(-14, 2) == 2);
+    assert(mcd(0, 4) == 4);
+    assert(mcd(-14, -2) == -2);
+}
 
-int mcd (int, int);
-
+/*Implementación*/
+int mcd(int a, int b) { return a % b == 0 ? b : mcd(b, a % b); }
