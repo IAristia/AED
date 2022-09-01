2c57b1ffd3ab27f24fe45bd9e2eb22d93977deb3
JuanPabloGalgano <102485023+JuanPabloGalgano@users.noreply.github.com>
2022-09-01 12:50:56 -0300

    Update main.cpp

--- Distancia entre puntos/main.cpp
+++ Distancia entre puntos/main.cpp
@@ -5,15 +5,11 @@
 /*
 Problema: Distancia entre dos puntos en el plano 
 */
-
-
 /*
  Punto = {(x,y)/x,yE R} = R^2
  Dis: Punto x Punto -> R / Dis( a , b ) = {raiz((ax-bx)^2+(ay-by)^2}
-
-
-}
  */
+
 bool AreNear(double, double, double = 0.1); //cambiando la tolerancia, tenemos que cambiar los resultados
 
 struct point{
