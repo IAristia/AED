b5c5c78c86d317ca01ac27ede75f660f69734443
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-09-01 11:59:03 -0300

    Update main.cpp

--- Distancia entre puntos/main.cpp
+++ Distancia entre puntos/main.cpp
@@ -11,7 +11,17 @@ Problema: Distancia entre dos puntos en el plano
  Punto = {(x,y)/x,yE R} = R^2
  Dis: Punto x Punto -> R / Dis( a , b ) = {raiz((ax-bx)^2+(ay-by)^2}
 
-
+    assert ( AreNear(Dis({1,1},{1,1}),0));
+    assert ( AreNear(Dis({-1,1},{1,-1}),2.8));
+    assert ( AreNear(Dis({-1,0},{1,0}), 2));
+    assert ( AreNear(Dis({0,-1}, {0,1}),2));
+    assert ( AreNear(Dis({1.0,1.0},{-1.0,-1.0}),2.8));
+    assert ( AreNear(Dis({3.2,1},{-4.5,3.2}),8));
+    assert ( AreNear(Dis({-2.3,-1.6},{-4.3,-1.3}),2));
+    assert ( AreNear(Dis({-2.3,1.6},{-4.3,1.3}),2));
+    assert ( AreNear(Dis({-2.3,-1.6},{4.3,1.3}),7.11));
+    assert ( AreNear(Dis({2.3,-1.6},{4.3,-1.3}),2));
+}
  */
 bool AreNear(double, double, double = 0.1); //cambiando la tolerancia, tenemos que cambiar los resultados
 
