d157b29c935b30cfc8164da4071871b3db364f88
JuanPabloGalgano <102485023+JuanPabloGalgano@users.noreply.github.com>
2022-10-20 10:06:47 -0300

    Update Dim1.cpp

--- 15-Dims/Dim 1/Dim1.cpp
+++ 15-Dims/Dim 1/Dim1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <array>
 
+
+// Venta* -> P -> Total
+// Venta = N
+
+int main () {
+
+   std::array <unsigned,12> total{0}; // dim 2
+
+   for (unsigned venta, mes; std::cin >> venta >> mes;)
+
+        total.at(mes) += venta;
+
+// std::cout << total << "\n" ; MAL, no se puede mostrar un arreglo
+
+// for (int i{0}; i<12; ++i) // i++ = i = i+1
+// std::cout << total.at(i) << "\n";
+
+    for (int totalDelMes : total)
+        std::cout << totalDelMes << "\n";
+
+}
