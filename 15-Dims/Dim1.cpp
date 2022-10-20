0633b176632e7cdf45fd6e19302702a52f575019
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-10-19 21:11:50 -0300

    Update Dim1.cpp

--- 15-Dims/Dim1.cpp
+++ 15-Dims/Dim1.cpp
@@ -7,7 +7,7 @@
 
 int main () {
 
-   std::array <unsigned,12> total{0}; // dim 2
+   std::array <unsigned,12> total{0}; // dim 1
 
    for (unsigned venta, mes; std::cin >> venta >> mes;)
 
@@ -16,7 +16,7 @@ int main () {
 // std::cout << total << "\n" ; MAL, no se puede mostrar un arreglo
 
 // for (int i{0}; i<12; ++i) // i++ = i = i+1
-// std::cout << total.at(i) << "\n";
+// std::cout << total.at(i) << "\n";  // funciona porque es un array de 1 dimensión.
 
     for (int totalDelMes : total)
         std::cout << totalDelMes << "\n";
