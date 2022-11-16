287bd5dd5c417cbfbb757b22c0f1062be2659e7d
JuanPabloGalgano <102485023+JuanPabloGalgano@users.noreply.github.com>
2022-11-16 16:55:19 -0300

    Update Dim3.cpp

--- 15-Dims/Dim 3/Dim3.cpp
+++ 15-Dims/Dim 3/Dim3.cpp
@@ -8,7 +8,7 @@ int main () {
 
     array<array<array<unsigned,12>,3>,4> total {};
     
-     for (unsigned venta, mes, vendedor, region; std::cin >> venta >> mes >> vendedor >> region;)
+     for (unsigned venta, mes, vendedor, region; std::cin >> venta >> mes >> vendedor >> region;){
 
         total.at(mes).at(vendedor).at(region) += venta;
 }
