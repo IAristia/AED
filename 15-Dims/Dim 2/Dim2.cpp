9b4a3eb8432353fcc50fb89fb695272f333d0afb
Iñaki <104172176+IAristia@users.noreply.github.com>
2022-11-16 16:43:06 -0300

    Update Dim2.cpp

--- 15-Dims/Dim 2/Dim2.cpp
+++ 15-Dims/Dim 2/Dim2.cpp
@@ -3,15 +3,16 @@
 
 int main () {
     
-std::array <std::array<unsigned,3>,12> total {};
+std::array <std::array<unsigned,12>,3> total {};
 
 for (unsigned venta, mes, vendedor; std::cin >> venta >> mes >> vendedor;) 
-            total.at(mes).at(vendedor) += venta;
 
-        for (auto vendedor: total) {
-            for (auto totaldelmes : vendedor)
-                std::cout << totaldelmes << '\t';
-             std::cout << '\n';
+            total.at(vendedor).at(mes) += venta;
+
+        for (auto vendedor : total) { // del arreglo total extrae un elemento (array de 12 unsigneds).
+            for (auto totaldelmes : vendedor){ // del arreglo vendedor extrae un elemento (1 unsigned (suma de ventas de un vendedor específico en un mes en específico))
+                std::cout << totaldelmes << '\t';}
+             std::cout << "\n--------------------------------------------------------\n";
 
         }
     }
