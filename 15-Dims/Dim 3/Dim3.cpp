9779185beedd9fdbf9300e0f15fc1e33107752a6
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-11-16 16:51:46 -0300

    Update Dim3.cpp

--- 15-Dims/Dim 3/Dim3.cpp
+++ 15-Dims/Dim 3/Dim3.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 #include <array>
-#include <fstream>
-#include "WriteBlock.h"
+
 
 using std::array;
 
 int main () {
 
-    array<array<array<unsigned,4>,3>,12> total {};
-
-    for (unsigned venta, mes, vendedor, region; std::cin >> venta >> mes >> vendedor >> region;)
-
-        total.at(mes).at(vendedor).at(region) += venta;
-
-        for (auto mes : total){
-            for (auto vendedor : mes) {
-                for (auto region: vendedor)
-                    std::cout << region << '\t';
-                std::cout << '\n';
-            }
-        std:: cout << "-------------------------\n";
-        }
-    auto filename("totales");
-    std::ofstream out(filename, std::ios::binary);
-    WriteBlock(out, total);    
-}
+    array<array<array<unsigned,12>,3>,4> total {};
