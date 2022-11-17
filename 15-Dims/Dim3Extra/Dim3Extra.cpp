eae6626e47671133740452e907454abf924d73e6
JuanPabloGalgano <102485023+JuanPabloGalgano@users.noreply.github.com>
2022-11-17 13:14:23 -0300

    Update Dim3Extra.cpp

--- 15-Dims/Dim3Extra/Dim3Extra.cpp
+++ 15-Dims/Dim3Extra/Dim3Extra.cpp
@@ -1,1 +1,12 @@
-:)
+
+// Agrega el valor m a la posicion n dentro del array a del tipo de dato Meses
+void Meses::Agregar (unsigned m){
+    a.at(n) = m, n++;
+}
+
+//Muestra por el stream salida contenido del dato del tipo Meses
+void Meses::Imprimir (std::ostream &salida){
+    for (unsigned i{}; i < n ; i++)
+        salida << a.at(i) << ' ';
+    salida<<'\n';
+}
