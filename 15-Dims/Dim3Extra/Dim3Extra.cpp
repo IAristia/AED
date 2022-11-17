1e5b6cf56080f4161cb1a3b6a722714495f71a9f
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-11-17 13:15:37 -0300

    Update Dim3Extra.cpp

--- 15-Dims/Dim3Extra/Dim3Extra.cpp
+++ 15-Dims/Dim3Extra/Dim3Extra.cpp
@@ -1,3 +1,24 @@
+//Obtiene el mayor monto
+unsigned GetMayorMonto (const Total& total){
+
+    unsigned mayormonto{};
+
+    for (unsigned  r{}; r<total.size(); r++) // 4
+        for (unsigned  v{}; v<total.at(0).size(); v++) // 3
+            for (unsigned  m{}; m<total.at(0).at(0).size(); m++) // 12
+
+                if(total.at(r).at(v).at(m) > mayormonto)
+                    mayormonto = total.at(r).at(v).at(m);
+
+    return mayormonto;
+}
+//Devuelve el/los meses con mayor venta
+Meses GetMesesConMayorVentas (const Total& total){
+
+    Meses mesesconmayorventa = GetMesesConMontoIgualA(total, GetMayorMonto(total));
+
+    return mesesconmayorventa;
+}
 
 // Agrega el valor m a la posicion n dentro del array a del tipo de dato Meses
 void Meses::Agregar (unsigned m){
