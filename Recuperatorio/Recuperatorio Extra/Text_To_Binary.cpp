2bb9155aa9b19d667885ad32d23ca00a78be543d
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-12-15 18:15:47 -0300

    Update Text_To_Binary.cpp

--- Recuperatorio/Recuperatorio Extra/Text_To_Binary.cpp
+++ Recuperatorio/Recuperatorio Extra/Text_To_Binary.cpp
@@ -1,1 +1,20 @@
+/*
+Alumnos: Galgano Juan Pablo, Aristia Iñaki, Mosquera Alfaro Francisco, Luberto Ezequiel
 
+Solución para lectura de datos en forma binaria
+
+ */
+#include <iostream>
+#include "BlockStream.h"
+
+int main(){
+
+    std::ofstream out("Traducido_T_B.bin", std::ios::binary);
+
+    for(unsigned aux;std::cin>>aux;)
+        WriteBlock(out, aux);
+
+    out.close();
+
+
+}
