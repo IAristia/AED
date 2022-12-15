defbba6019fea89aa077fd032fcd231052daeb6c
fmosqueraalfaro <103387959+fmosqueraalfaro@users.noreply.github.com>
2022-12-15 19:01:23 -0300

    Update Binary_To_Text.cpp

--- Recuperatorio/Binary_To_Text.cpp
+++ Recuperatorio/Binary_To_Text.cpp
@@ -2,31 +2,9 @@
 Alumnos: Galgano, Aristia, Mosquera, Luberto
 Solución para lectura de datos en forma binaria
 
-Objetivo de entrega:
-
-Recibir feedback si el proceso de lectura y escritura de datos es el correcto y coincide con lo solicitado por el docente.
-
 */
 #include <iostream>
 #include "BlockStream.h"
-#include <cassert>
-#include <array>
-
-using std::array;
-using Total = array<array<array<unsigned,12>,3>,4>;
-
-/*Creamos estructura que es una venta mensual, asociada a un vendedor, región y mes.
-Esta estructura la vamos a utilizar para leer y también la utilizamos para la construcción
-de las muestras para las pruebas*/
-
-void CargarDatos(Total&, std::ifstream&);
-struct venta{
-    unsigned monto;
-    unsigned mes;
-    unsigned vendedor;
-    unsigned region;
-    void print_f(std::ofstream&);
-};
 
 int main(){
 
@@ -41,11 +19,4 @@ int main(){
     out.close();
 
 
-}
-
-void venta::print_f(std::ofstream& out){
-    out<<monto<<' ';
-    out<<mes<<' ';
-    out<<vendedor<<' ';
-    out<<region<<'\n';
 }
